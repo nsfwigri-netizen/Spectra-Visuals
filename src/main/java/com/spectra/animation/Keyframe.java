0bf1ad681f37b37e259f31c053443c23a4dc2de3
nsfwigri-netizen <nsfwigri@gmail.com>
2026-09-13 16:17:21 +0400

    Build current Spectra project from uploaded archive

--- src/main/java/com/spectra/animation/Keyframe.java
+++
@@ -1,5 +0,0 @@
-package com.spectra.animation;
-
-/** Один ключевой кадр: момент времени (в тиках), значение и easing к следующему кадру. */
-public record Keyframe(float time, float value, Easing easing) {
-}
